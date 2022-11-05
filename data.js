26996687b8706651606a2ee05447427887170f11
Pinkydvs <116494570+Pinkydvs@users.noreply.github.com>
2022-11-05 07:20:29 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,7 @@
+let favMovies = {title: ["Game of Thrones", "The War of the Roses", "The Game", "Warcraft"],  year: [2011, 1989, 1997, 2016], 
+                rating: [9.2, 6.8, 7.7, 6.7], 
+                description: ["Nine noble families fight for control over the lands ofWesteros, while an ancient enemy returns after being dormant for millennia.",
+                             "A married couple tries everything to drive each other out of the house in a vicious divorce battle."
+                             "After a wealthy San Francisco banker is given an opportunity to participate in a mysterious game, his life is turned upside down as he begins to question if it might really be a concealed conspiracy to destroy him.",
+                             "As an Orc horde invades the planet Azeroth using a magic portal, a few human heroes and dissenting Orcs must attempt to stop the true evil behind this war."]};
+
